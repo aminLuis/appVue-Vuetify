5feb52bdf846c680c65ade913e94e48fe2fb9a1b
aminLuis <aminluisamin@gmail.com>
2021-10-23 21:56:08 -0500

    Toolbar, iconos y navigation drawer implementados

---
+++ src/components/articulo/inicio.vue
@@ -0,0 +1,89 @@
+<template>
+    <div>
+          <v-toolbar color="purple" app>
+
+              <v-toolbar-side-icon>
+                 <v-btn icon @click="drawer = !drawer">
+                      <v-icon>menu</v-icon>
+                 </v-btn>
+              </v-toolbar-side-icon>
+              
+              <v-toolbar-title> Producto </v-toolbar-title>
+
+               <v-spacer></v-spacer>
+
+               <v-btn style="margin-right:5px" class="success" elevation="2" small>
+                   Entry
+               </v-btn>
+               
+               <v-btn class="error" elevation="2" small>
+                   Logout
+               </v-btn>
+
+          </v-toolbar>    
+
+      <v-navigation-drawer app v-model="drawer" temporary>
+         
+          <v-layout mt-4 column align-center>
+
+              <v-flex>
+
+                  <v-avatar>
+                      <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+                  </v-avatar>
+
+              </v-flex>
+              <v-flex>
+                  <p class="white--text mt-3 headline">Nombre usuario</p>
+              </v-flex>
+
+          </v-layout>
+
+          <v-layout>
+
+              <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-title>Foo</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Bar</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Fizz</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Buzz</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+
+          </v-layout>
+
+      </v-navigation-drawer>
+      
+
+    </div>
+</template>
+
+<script>
+
+
+export default {
+name : 'inicio',
+data() {
+    return {
+       drawer: true
+    };
+},    
+}
+</script>
